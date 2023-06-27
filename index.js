22ed95bda742b973792172686351d96a0749bae8
moneymovesmedia <business.mmmedia@gmail.com>
2023-06-27 19:26:19 +0300

    pls worrkk broo

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
-import TikTok from 'tiktok-search';
+const TikTokSearch = require('tiktok-search');
 
-const searchTikTok = async (query) => {
+const ACCOUNTS = ['iamturnbull', 'liamoimfmxo']; // Replace with the TikTok account usernames you want to fetch
+
+const displayAccountData = (data) => {
+  const resultsContainer = document.getElementById('results');
+  resultsContainer.innerHTML = '';
+
+  data.forEach((account) => {
+    const accountDiv = document.createElement('div');
+    accountDiv.innerHTML = `
+      <h3>${account.username}</h3>
+      <p>Total Views: ${account.stats.videoCount}</p>
+      <p>Total Followers: ${account.stats.followerCount}</p>
+      <p>Last Post: ${account.latestPost.time}</p>
+    `;
+    resultsContainer.appendChild(accountDiv);
+  });
+};
+
+const fetchAccountData = async () => {
   try {
-    const posts = await TikTokScraper.search(query);
-    console.log(posts);
+    const promises = ACCOUNTS.map((account) => TikTokSearch.getUserProfile(account));
+    const data = await Promise.all(promises);
+    displayAccountData(data);
   } catch (error) {
-    console.error('Error searching TikTok:', error);
+    console.error('Error fetching account data:', error);
   }
 };
 
-searchTikTok('https://www.tiktok.com/@liamoimfmxo/');
+fetchAccountData();
