b5a31d74c2295c92f1a4c8c2f9fc2023d8eb3a20
moneymovesmedia <business.mmmedia@gmail.com>
2023-06-27 18:10:31 +0300

    Second

---
+++ index.js
@@ -0,0 +1,38 @@
+import axios from 'axios';
+const ACCESS_TOKEN = 'awupmyx41hmu1j2n';
+const ACCOUNTS = ['iamturnbull', 'liamoimfmxo']; // Replace with the TikTok account usernames you want to fetch
+
+const displayAccountData = (data) => {
+  const resultsContainer = document.getElementById('results');
+  resultsContainer.innerHTML = '';
+
+  data.forEach((account) => {
+    const accountDiv = document.createElement('div');
+    accountDiv.innerHTML = `
+      <h3>${account.username}</h3>
+      <p>Total Views: ${account.stats.totalViews}</p>
+      <p>Total Likes: ${account.stats.totalLikes}</p>
+      <p>Last Post: ${account.stats.lastPost}</p>
+    `;
+    resultsContainer.appendChild(accountDiv);
+  });
+};
+
+const fetchAccountData = async () => {
+  try {
+    const promises = ACCOUNTS.map((account) =>
+      axios.get(`https://open.tiktokapis.com/v1/user/${account}/stats`, {
+        headers: {
+          Authorization: `Bearer ${ACCESS_TOKEN}`,
+        },
+      })
+    );
+    const responses = await Promise.all(promises);
+    const data = responses.map((response) => response.data.user);
+    displayAccountData(data);
+  } catch (error) {
+    console.error('Error fetching account data:', error);
+  }
+};
+
+fetchAccountData();
