d63deba2d769f5dc077add5f9512fba1a8fe16f1
moneymovesmedia <business.mmmedia@gmail.com>
2023-06-27 19:31:29 +0300

    lol

--- index.js
+++ index.js
@@ -1,31 +1,4 @@
-import { getUserProfile } from 'tiktok-search';
-
-const ACCOUNTS = ['iamturnbull', 'liamoimfmxo']; // Replace with the TikTok account usernames you want to fetch
-
-const displayAccountData = (data) => {
-  const resultsContainer = document.getElementById('results');
-  resultsContainer.innerHTML = '';
-
-  data.forEach((account) => {
-    const accountDiv = document.createElement('div');
-    accountDiv.innerHTML = `
-      <h3>${account.username}</h3>
-      <p>Total Views: ${account.stats.videoCount}</p>
-      <p>Total Followers: ${account.stats.followerCount}</p>
-      <p>Last Post: ${account.latestPost.time}</p>
-    `;
-    resultsContainer.appendChild(accountDiv);
-  });
-};
-
-const fetchAccountData = async () => {
-  try {
-    const promises = ACCOUNTS.map((account) => getUserProfile(account));
-    const data = await Promise.all(promises);
-    displayAccountData(data);
-  } catch (error) {
-    console.error('Error fetching account data:', error);
-  }
-};
-
-fetchAccountData();
+const TikTok = require("tiktok-search");
+ 
+TikTok.getUser('iiamturnbull')
+.then(console.log);
